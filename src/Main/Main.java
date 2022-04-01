71d86e162fdd797c3130ad27d3eed40cb29d6bc8
Ale <alejandromaximo2000@gmail.com>
2022-04-01 18:31:00 -0300

    test commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,11 +7,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        PersonajePrincipal personaje1 = new PersonajePrincipal(100, 25, "unnamed", 0, 0);
+        PersonajePrincipal personaje1 = new PersonajePrincipal(100, 25, "unnamed", 0, 0, true, 5);
         System.out.println("Como se llama tu personaje?");
         personaje1.nombre = sc.nextLine();
 
-        System.out.println(personaje1);
 
+        while(personaje1.vida > 0){
+            if(personaje1.movimientos >0) {
+                personaje1.Move();
+                personaje1.ShowStats();
+            }else{
+                personaje1.vida = 0;
+                personaje1.Die();
+                break;
+            }
+        }
+
+    }
+
+    public static void ClearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
--- src/Personajes/PersonajePrincipal.java
+++ src/Personajes/PersonajePrincipal.java
@@ -2,10 +2,13 @@ package Personajes;
 
 import java.util.Scanner;
 
+import static Main.Main.ClearScreen;
+
 public class PersonajePrincipal {
 
         // Atributos
         public int vida;
+        public int movimientos;
         public int daño;
         public String nombre;
         int pos_coordY;
@@ -13,58 +16,70 @@ public class PersonajePrincipal {
         public boolean vivo;
 
         // Constructor
-        public PersonajePrincipal(int vida, int daño, String nombre, int pos_coordY, int pos_coordX){
+        public PersonajePrincipal(int vida, int daño, String nombre, int pos_coordY, int pos_coordX,boolean vivo, int movimientos){
             this.vida = vida;
             this.daño = daño;
             this.nombre = nombre ;
             this.pos_coordY = pos_coordY;
             this.pos_coordX = pos_coordX;
+            this.movimientos = movimientos;
+            this.vivo = vivo;
         }
 
         // Metodos
-        public void moverse(){
+        public void Move(){
             Scanner sc = new Scanner(System.in);
             System.out.println("Movete hacia alguna direccion:");
             String direccion = sc.nextLine();
             direccion = direccion.toLowerCase();
+            ClearScreen();
 
 
             switch(direccion){
                 case "arriba":
                     this.pos_coordY = pos_coordY+1;
-                    System.out.println("Te moviste 1 posicion hacia arriba");
+                    System.out.println(this.nombre+", te moviste 1 posicion hacia arriba");
                     System.out.println("Estas en X: "+pos_coordX+ " Y: "+pos_coordY);
-                    this.vida = vida-25;
+                    this.movimientos--;
                     break;
 
                 case "abajo":
                     this.pos_coordY = pos_coordY-1;
-                    System.out.println("Te moviste 1 posicion hacia abajo");
+                    System.out.println(this.nombre+", te moviste 1 posicion hacia abajo");
                     System.out.println("Estas en X: "+pos_coordX+ " Y: "+pos_coordY);
+                    this.movimientos--;
                     break;
 
                 case "derecha":
                     this.pos_coordX = pos_coordX+1;
-                    System.out.println("Te moviste 1 posicion hacia la derecha");
+                    System.out.println(this.nombre+", te moviste 1 posicion hacia la derecha");
                     System.out.println("Estas en X: "+pos_coordX+ " Y: "+pos_coordY);
+                    this.movimientos--;
                     break;
 
                 case "izquierda":
                     this.pos_coordX = pos_coordX-1;
-                    System.out.println("Te moviste 1 posicion hacia la izquierda");
+                    System.out.println(this.nombre+", te moviste 1 posicion hacia la izquierda");
                     System.out.println("Estas en X: "+pos_coordX+ " Y: "+pos_coordY);
+                    this.movimientos--;
                     break;
             }
+
+            System.out.println("----------------------------------");
         }
 
-        public void Morir(){
-            System.out.println("Te moriste");
+        public void Die(){
+            System.out.println("Vida: "+this.vida+", te moriste");
         }
 
-        private void ShowStats(){
+        public void ShowStats(){
+            System.out.println("* Datos del personaje *");
+            System.out.println(this.nombre);
             System.out.println("Vida: "+ this.vida);
+            System.out.println("Te quedan " +this.movimientos+ " movimientos");
+            System.out.println("----------------------------------");
         }
-        
+
     }
 
 
